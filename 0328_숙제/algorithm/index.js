4556b276787df4da08a0a90427c83f15fcf7dbda
hyunsangwon <hyunsangwon93@gmail.com>
2022-04-02 19:04:30 +0900

    feat : 숙제 답안 업로드 완료

--- 0328_숙제/algorithm/index.js
+++ 0328_숙제/algorithm/index.js
@@ -7,7 +7,7 @@ let len =  array.length;
 for(let i=len; i > 0; i--){
     for (let j=0; j < (i - 1); j++) {
         if (array[j] > array[j+1]) {
-            let temp = array[j];
+            let temp = array[j]; //temp를 사용하지 않으면 i+1번째 배열에 i번째 원소(index 값)를 swap할 수 없다.
             array[j] = array[j+1];
             array[j+1] = temp;
             isSwap = true;          
