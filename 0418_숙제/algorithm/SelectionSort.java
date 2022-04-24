7a1c274fa343a05e7866e7c452a603b12b590fb8
hyunsangwon <hyunsangwon93@gmail.com>
2022-04-24 17:32:09 +0900

    fix : 선택정열 알고르짐 일부 수정

--- 0418_숙제/algorithm/SelectionSort.java
+++ 0418_숙제/algorithm/SelectionSort.java
@@ -5,11 +5,12 @@ public class SelectionSort {
 	public static void main(String[] args) {
 		int array[] = {9, 6, 7, 3, 5};
 		boolean isChange = false; //교환 여부 판단!
-
-		for(int i=0; i<(array.length-1); i++){ //4회전 세팅
+		int arrLen = array.length;
+		for(int i=0; i<(arrLen-1); i++){ //4회전 세팅
+			isChange = false;
 			int min = array[i];//0번째 원소를 초기값으로 세팅!
 			int index = 0; //최솟값 위치변수.
-			for(int j=i+1; j<array.length; j++){
+			for(int j=i+1; j<arrLen; j++){ //최솟값을 찾자!
 				int jIndex = array[j];
 				if(min > jIndex){
 					min = jIndex;
@@ -22,7 +23,7 @@ public class SelectionSort {
 				array[i] = min;
 				array[index] = temp;
 			}
-            if(!isChange) break;
+            if(!isChange) break; //더이상 찾을께 없으면 멈추자
 		}//outer-for end
 		
 		
